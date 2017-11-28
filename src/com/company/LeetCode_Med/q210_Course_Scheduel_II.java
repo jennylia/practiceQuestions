33ff53061fabccabf4b46f7d76c91bf85fb146c9
Jenny Lian <jennylian100@gmail.com>
2017-11-27 19:31:16 -0800

    added some notes, ponder on ye way home

--- src/com/company/LeetCode_Med/q210_Course_Scheduel_II.java
+++ src/com/company/LeetCode_Med/q210_Course_Scheduel_II.java
@@ -13,6 +13,15 @@ public class q210_Course_Scheduel_II {
     public static int[] findOrder(int numCourses, int[][] prerequisites) {
         int[] courseList = new int[1000];
         return courseList;
-
     }
 }
+
+/*Notes
+* https://github.com/mission-peace/interview/blob/master/src/com/interview/graph/TopologicalSort.java
+* https://www.youtube.com/watch?v=ddTC4Zovtbc
+*
+* Mr Tushar Roy has this information very well done, with a deque(childless nodes) and a set (visited)
+*
+* https://leetcode.com/problems/course-schedule-ii/description/
+*
+* Original question.*/
